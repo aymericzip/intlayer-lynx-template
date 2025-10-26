f078ea82ec43f9796a820b081fbfad073cab1ee7
Aymeric PINEAU <ay.pineau@gmail.com>
2025-10-27 00:18:05 +0100

    feat: update template

--- src/components/LocaleSwitcher.tsx
+++ src/components/LocaleSwitcher.tsx
@@ -1,32 +1,32 @@
-import { type FC } from "react";
 import { getLocaleName } from "intlayer";
+import type { FC } from "react";
 import { useLocale } from "react-intlayer";
 
 export const LocaleSwitcher: FC = () => {
-  const { setLocale, availableLocales, locale } = useLocale();
+	const { setLocale, availableLocales, locale } = useLocale();
 
-  return (
-    <view
-      style={{
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "center",
-        alignItems: "center",
-        gap: 10,
-      }}
-    >
-      {availableLocales.map((localeEl) => (
-        <text
-          key={localeEl}
-          style={{
-            color: localeEl === locale ? "#fff" : "#888",
-            fontSize: "12px",
-          }}
-          bindtap={() => setLocale(localeEl)}
-        >
-          {getLocaleName(localeEl)}
-        </text>
-      ))}
-    </view>
-  );
+	return (
+		<view
+			style={{
+				display: "flex",
+				flexDirection: "row",
+				justifyContent: "center",
+				alignItems: "center",
+				gap: 10,
+			}}
+		>
+			{availableLocales.map((localeEl) => (
+				<text
+					key={localeEl}
+					style={{
+						color: localeEl === locale ? "#fff" : "#888",
+						fontSize: "12px",
+					}}
+					bindtap={() => setLocale(localeEl)}
+				>
+					{getLocaleName(localeEl)}
+				</text>
+			))}
+		</view>
+	);
 };
