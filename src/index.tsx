d73d318ef0c09046f7d9b161f4055ac15f367280
Aymeric PINEAU <ay.pineau@gmail.com>
2025-10-27 00:17:57 +0100

    feat: update template

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,9 @@
+import "@formatjs/intl-locale/polyfill";
+import "@formatjs/intl-displaynames/polyfill";
+import "@formatjs/intl-displaynames/locale-data/en";
+import "@formatjs/intl-displaynames/locale-data/fr";
+import "@formatjs/intl-displaynames/locale-data/es";
+
 import { root } from "@lynx-js/react";
 
 import { App } from "./App.js";
